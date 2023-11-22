f7d463f3236f5dce3f295d913920ecb0feaf0912
Max Petzold <maxpetzold581@gmail.com>
2023-11-22 12:00:37 +0100

    lagde en kontakt oss side med emailene til alle oss

---
+++ bakka-baas-nettside/src/pages/kontakt/carlantonkontaktboks.jsx
@@ -0,0 +1,30 @@
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import Link from "next/link";
+
+export default function KontaktBokser() {
+  const gridboxStyles =
+    "flex items-center gap-5 w-30 h-30 border border-white pl-2 p-3 rounded-md";
+  const iconStyle = "w-16 h-16";
+  const mailIcons = "w-8 h-8 hover:text-green-400";
+  const nameStyles = "text-3xl mt-7";
+  return (
+    <div className={gridboxStyles}>
+      <div className="h-full flex flex-col justify-center">
+        <FaUser className={iconStyle} />{" "}
+      </div>
+
+      <div className="flex flex-col gap-1 mt">
+        <span className={nameStyles}>Carl Anton</span>
+        <span>kontakt på:</span>
+        <span className="h-[1px] w-11/12 bg-white"></span>
+        <div className="flex gap-2 items-center ">
+          <Link href="mailto:cadaa008@osloskolen.no">
+            <MdEmail className={mailIcons} />
+          </Link>{" "}
+          <span>cadaa008@osloskolen.no</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- bakka-baas-nettside/src/pages/kontakt/index.jsx
+++ bakka-baas-nettside/src/pages/kontakt/index.jsx
@@ -1,11 +1,24 @@
 import Navbar from "../components/Navbar";
+import MaxKontaktBoks from "@/pages/kontakt/maxkontaktboks.jsx";
+import AttilaKontaktBoks from "@/pages/kontakt/attilakontaktboks.jsx";
+import CarlAntonKontaktBoks from "@/pages/kontakt/carlantonkontaktboks.jsx";
+import JonasKontaktBoks from "@/pages/kontakt/jonaskontaktboks.jsx";
 
 export default function Kontakt() {
   return (
     <div className="bg-1 h-screen">
       <Navbar />
-      <h1>kontakt oss</h1>
-      <p>kontakt oss på placeholder</p>
+
+      <div className="flex flex-col w-screen h-full justify-center items-center gap-20">
+        {" "}
+        <h1 className="text-8xl">Team members</h1>
+        <section className="grid grid-cols-2 gap-8 w-1/2 h-1/2">
+          <MaxKontaktBoks />
+          <AttilaKontaktBoks />
+          <CarlAntonKontaktBoks />
+          <JonasKontaktBoks />
+        </section>
+      </div>
     </div>
   );
 }
