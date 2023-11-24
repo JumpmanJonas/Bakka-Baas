ebd8259cc6dcc2e2120b5f0820f101fe19e4f6a7
JumpmanJonas <75727702+JumpmanJonas@users.noreply.github.com>
2023-11-24 12:30:29 +0100

    fixed

--- bakka-baas-nettside/editing_a_json_file/index.js
+++ bakka-baas-nettside/editing_a_json_file/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const fs = require('fs');
+const { json } = require('stream/consumers');
 const app = express();
 const port = 3001;
 
@@ -15,19 +16,45 @@ app.get('/', function(request, response){ // shows "Hello World!" on the screen
 app.get('/info', function(request, response){ // shows the places.json file
     const data = fs.readFileSync('places.json', 'utf8');
     const jsonData = JSON.parse(data);
-    console.log(jsonData['bås1']['occupiedBy'])
     response.send(jsonData)
 });
 
-app.get('/info/:name', function(request, response){
-    const newName = request.params["name"]
+
+app.post('/info/:name-:startDate-:endDate', function(request, response){
+
+    const newName = request.params.name
+    const startD = request.params.startDate
+    const endD = request.params.endDate
     fs.readFile('places.json', 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err);
         } else {
         obj = JSON.parse(data); //now it an object
-        obj['bås1']['bookings'].push({"date": [newName, "date2"]}); //add some data
-        json = JSON.stringify(obj); //convert it back to json
-        fs.writeFile('places.json', json, 'utf8', callback); // write it back 
+        console.log(obj['båser']['bås1']['bookings'])
+        let booking3 = {
+            "name": newName, 
+            "start": startD,
+            "end": endD
+            
+        }
+        console.log(booking3)
+        obj['båser']['bås1']['bookings'].push(booking3) //add some data
+        console.log(obj['båser']['bås1']['bookings'])
+        
+
+
+        // Convert the JSON object to a string
+        const jsonString = JSON.stringify(obj, null, 2);
+        const filePath = 'places.json';
+        // Write the JSON string to the file
+        fs.writeFile(filePath, jsonString, (err) => {
+            if (err) {
+            console.error('Error writing to file:', err);
+            } else {
+            console.log(`JSON object has been written to ${filePath}`);
+            }
+        });
     }});
+    response.send("Posted")
+
 })
